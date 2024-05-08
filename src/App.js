a059e0af409c62f4266961c672c08080668fb46c
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2024-05-08 09:05:43 -0300

    a

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Boton from './components/Boton/index.jsx';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Boton />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Boton/index.jsx
@@ -0,0 +1,4 @@
+const Boton = () => {
+    return <button onClick={() => alert("Hola")}>Hola</button>
+}
+export default Boton;
